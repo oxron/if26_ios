5599e6822e014abcb4d2a2ebdf1518cb5a11a1d7
Laura <laura.haegel@gmail.com>
2019-11-07 17:51:43 +0100

    changement programme base - with SwiftUI

--- minimalist/minimalist/AppDelegate.swift
+++ minimalist/minimalist/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  minimalist
+//  Minimalist
 //
 //  Created by Leuleu on 07/11/2019.
 //  Copyright © 2019 Leuleu. All rights reserved.
@@ -42,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "minimalist")
+        let container = NSPersistentContainer(name: "Minimalist")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.

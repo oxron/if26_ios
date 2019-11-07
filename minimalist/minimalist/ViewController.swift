24d9bc44288aaa281f2820081d068adf922caf76
Laura <laura.haegel@gmail.com>
2019-11-07 17:06:28 +0100

    creation base application

---
+++ minimalist/minimalist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  minimalist
+//
+//  Created by Leuleu on 07/11/2019.
+//  Copyright © 2019 Leuleu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

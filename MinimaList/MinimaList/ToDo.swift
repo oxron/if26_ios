d1e213dcedf3caad84072e451ba3ad117b9252f8
Laura <laura.haegel@gmail.com>
2019-11-08 05:21:44 +0100

    table view + ajoute task

---
+++ MinimaList/MinimaList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  MinimaList
+//
+//  Created by Leuleu on 07/11/2019.
+//  Copyright © 2019 Leuleu. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    var name = ""
+}
